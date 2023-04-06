759bbb84c54ad24295eb13ce7e1a6061ab3a1339
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-05 22:55:28 -0700

    added admin.html file

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
 function displayProduct(product) {
     let card = document.createElement("li")
-    card.className = "card col-2 p-0 m-2"
+    card.className = "card col-2 m-2"
     card.innerHTML = `
     <img src="${product.image}" class="card-img-top" alt="${product.title}">
     <div class="card-body">
         <h5 class="card-title">${product.title}</h5>
         <p class="card-text">${product.description}</p>
-        <h6 class="card-title">Ksh: ${product.price}</h6>
-        <a href="#" class="btn">Add to Cart</a>
+        <h6 class="card-title">
+            Ksh:<span>${product.price}</span>
+        </h6>
+        <a class="btn btn-primary">Buy</a>
     </div>
     `
+    //add the card to the DOM
     document.querySelector("#item-list").append(card)
 }
 
-//fetch 
-//GET request
-
-//POST create
-//DELETE
-//UPDATE
-
-function fetchProducts() {
-    fetch("http://localhost:3000/products")
+//fetch product data
+function getProducts() {
+    fetch(" http://localhost:3000/products")
     .then(res => res.json())
     .then((products) => {
-        products.forEach((product) =>{
+        // console.log(products)
+        products.forEach((product) => {
             displayProduct(product)
         })
     })
 }
-fetchProducts()
+getProducts()
